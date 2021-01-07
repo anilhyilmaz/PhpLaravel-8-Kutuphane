276c8f562c8a0e56f194b33d54ece4e734173e93
ANIL HAYDAR YILMAZ <anilhyilmaz@gmail.com>
2021-01-08 00:54:41 +0300

    Product Detail Rich Text Editor Zengin Metin Editorü Eklendi (summernote)

--- resources/views/admin/product_edit.blade.php
+++ resources/views/admin/product_edit.blade.php
@@ -2,6 +2,14 @@
 
 @section('title','Admin Panel Home Page')
 
+@section('javascript')
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+@endsection
+
 @section('content')
 
     <!-- main content start -->
@@ -75,7 +83,14 @@
                         </div>
                         <div class="form-group">
                             <label>Detail</label>
-                            <input type="text" name="detail" value="{{$data->detail}}" class="form-control input-style">
+                            <textarea id="summernote" value="{{$data->detail}}" name="detail">{{$data->detail}}</textarea>
+                            <script>
+                                $('#summernote').summernote({
+                                    placeholder: 'Hello Bootstrap 4',
+                                    tabsize: 2,
+                                    height: 100
+                                });
+                            </script>
                         </div>
                         <div class="form-group">
                             <label>Slug</label>
